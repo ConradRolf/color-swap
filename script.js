b6d7c438a071729b2753c1f946b891a3daf1e4e8
Conrad Rolf <williamconradrolf@gmail.com>
2023-05-08 15:05:20 -0500

    added color changing, added remove button, enabled functionallity for increasing counter

--- script.js
+++ script.js
@@ -3,4 +3,33 @@ $(document).ready(onReady);
 function onReady() {
     console.log('We have jQuery! 💲💲💲');
 
+    $('#generate-button').on('click', newDiv);
+    $('body').on('click', '#yellow-button', turnYellow);
+    $('body').on('click', '#delete-button', rmvRow);
+    // $('#generate-button').on('click', upCount)
 }
+
+let buttonCounter = 0
+
+function newDiv(){
+    buttonCounter += 1;
+    $('body').append(`
+        <div class="redDiv">
+            <p><span>${buttonCounter}</span></p>
+            <button id="yellow-button">Yellow</button>
+            <button id="delete-button">Delete</button>
+        </div>
+    `)
+}
+
+function turnYellow(){
+    $(this).parent().css("background", "yellow");
+}
+
+function rmvRow(){
+    $(this).closest('div').remove();
+}
+
+// function upCount(){
+    
+// }
